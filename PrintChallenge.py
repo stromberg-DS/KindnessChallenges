23d4a18eba65b952e395d50c7d5f5f591031862a
DStromberg <strombergundy@gmail.com>
2024-05-06 15:00:40 -0600

    Code loops - allows enter to be pressed many times

--- PrintChallenge.py
+++ PrintChallenge.py
@@ -1,15 +1,25 @@
+#Could make this work more seemlessly using the os library to
+#   find the files, count them, etc to make the program more flexible.
+#   More info here: https://realpython.com/working-with-files-in-python/
+
+
 import os
 import random
 
-difficultyFolder = ["1_Easy\\", "2_Medium\\", "3_Hard\\"]
-numFilesInFolder = [11, 12, 6]
+while True:
+    input("Press enter")
+    textTypeFolder = ["Challenges\\", "Quotes\\"]
+    difficultyFolder = ["1_Easy\\", "2_Medium\\", "3_Hard\\"]
+    numFilesInFolder = [11, 12, 6]
 
-difficulty = random.randint(0,2)
-filename = random.randint(1, numFilesInFolder[difficulty])
+    difficulty = random.randint(0,2)
+    filename = random.randint(1, numFilesInFolder[difficulty])
 
-pathName = "C:\\Users\\dstromberg\\Documents\\Code\\SciKi\\KindnessChallenges\\Challenges\\"
+    pathName = "C:\\Users\\dstromberg\\Documents\\Code\\SciKi\\KindnessChallenges\\Challenges\\"
 
-print(difficulty)
-print(filename)
+    print(difficulty)
+    print(filename)
 
-os.startfile(pathName + difficultyFolder[difficulty] + str(filename) + ".txt", "print")
+    os.startfile(pathName + difficultyFolder[difficulty] + str(filename) + ".txt", "print")
+    #o s.startfile(pathName + difficultyFolder[difficulty] + str(filename) + ".txt", "print")
+    
