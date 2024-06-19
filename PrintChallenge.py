930585723aec47cca536c5c7b8c513f6e8c20fee
DStromberg <strombergundy@gmail.com>
2024-06-19 10:42:48 -0600

    Added setup PDF

--- PrintChallenge.py
+++ PrintChallenge.py
@@ -3,14 +3,26 @@
 #   More info here: https://realpython.com/working-with-files-in-python/
 
 
+import threading
 import os
+from os import system
 import random
+import pygetwindow as gw
+import time
+
+system("title " + "Random Acts of Kindness")
+
+lastFocusTime = time.time()
+print(time.time())
 
 #Update the path below so it goes to the real location of your
 #   challenges folder. Make sure to swap all single backslashes (\)
 #   for double backslashes (\\). Make sure you have two tacked
 #   onto the end  ("...Challenges\\")
-pathName = "C:\\Users\\dstromberg\\Documents\\Code\\SciKi\\KindnessChallenges\\Challenges\\"
+pathName = "C:\\Users\\Visitors\\OneDrive\\KindnessChallenges\\Challenges\\"
+
+# Use below for DS laptop
+##pathName = "C:\\Users\\dstromberg\\OneDrive\\KindnessChallenges\\Challenges\\"
 
 textTypeFolder = ["Challenges\\", "Quotes\\"]
 difficultyFolder = ["1_Easy\\", "2_Medium\\", "3_Hard\\"]
@@ -20,6 +32,12 @@ os.startfile(pathName + "StartupPrint.txt", "print")
 
 while True:
     input("Press enter")
+    currentTime = time.time()
+
+##    if (time.time() - lastFocusTime) > 2:
+##        print("Focus Time!")
+##        lastFocusTime = time.time()
+##    
 
     difficulty = random.randint(0,2)
     filename = random.randint(1, numFilesInFolder[difficulty])
@@ -27,6 +45,4 @@ while True:
     print(difficulty)
     print(filename)
 
-    os.startfile(pathName + difficultyFolder[difficulty] + str(filename) + ".txt", "print")
-    #o s.startfile(pathName + difficultyFolder[difficulty] + str(filename) + ".txt", "print")
-    
+    os.startfile(pathName + difficultyFolder[difficulty] + str(filename) + ".txt", "print")    
