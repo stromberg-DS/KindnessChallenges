1b824df4952dd325a0250d23abfd88bb5e5c3638
DStromberg <strombergundy@gmail.com>
2024-07-09 15:09:58 -0600

    Updated main code from test

--- PrintChallenge.py
+++ PrintChallenge.py
@@ -10,39 +10,75 @@ import random
 import pygetwindow as gw
 import time
 
-system("title " + "Random Acts of Kindness")
+timeToPrint = False
+QUOTES = 0
+EASY = 1
+MEDIUM = 2
+HARD = 3
+
+##############
+#Change this to adjust what difficulty level you want
+DIFFICULTY_LEVEL = MEDIUM
+##############
+
+def waitForInput():
+    input("Press Enter")
+    print("Sending to Printer!")
+    timeToPrint = True
+    
+#Name the window for reference later
+system("title " + "Random Acts of Kindness")    
 
 lastFocusTime = time.time()
 print(time.time())
 
-#Update the path below so it goes to the real location of your
-#   challenges folder. Make sure to swap all single backslashes (\)
-#   for double backslashes (\\). Make sure you have two tacked
-#   onto the end  ("...Challenges\\")
-pathName = "C:\\Users\\Visitors\\OneDrive\\KindnessChallenges\\Challenges\\"
+# All of these paths go to a OneDrive folder that I have set up
+#     This way, I can easily update the challenges remotely and
+#     all of the computers will pull from the updated folder.
+#     NOTE: Make sure to swap all single backslashes (\)
+#     for double backslashes (\\). Make sure you have two tacked
+#     onto the end  ("...Challenges\\")
+pathName = "C:\\Users\\dstromberg\\OneDrive\\KindnessChallenges\\"
+quotePath = pathName + "Quotes\\"
+easyPath = pathName + "Challenges\\1_Easy\\"
+medPath = pathName + "Challenges\\2_Medium\\"
+hardPath = pathName + "Challenges\\3_Hard\\"
+currentPath = [quotePath, easyPath, medPath, hardPath]
 
-# Use below for DS laptop
-##pathName = "C:\\Users\\dstromberg\\OneDrive\\KindnessChallenges\\Challenges\\"
+quoteList = os.listdir(quotePath)
+easyList = os.listdir(easyPath)
+medList = os.listdir(medPath)
+hardList = os.listdir(hardPath)
+currentList = [quoteList, easyList, medList, hardList]
+
+print(len(quoteList), " Quotes")
+print(quoteList)
+print()
+print(len(easyList), " Easy")
+print(easyList)
+print()
+print(len(medList), " Medium")
+print(medList)
+print()
+print(len(hardList), " Hard")
+print(hardList)
 
 textTypeFolder = ["Challenges\\", "Quotes\\"]
 difficultyFolder = ["1_Easy\\", "2_Medium\\", "3_Hard\\"]
 numFilesInFolder = [11, 12, 6]
 
-os.startfile(pathName + "StartupPrint.txt", "print")
+os.startfile(pathName + "Challenges\\StartupPrint.txt", "print")
 
 while True:
     input("Press enter")
     currentTime = time.time()
 
-##    if (time.time() - lastFocusTime) > 2:
-##        print("Focus Time!")
-##        lastFocusTime = time.time()
-##    
-
-    difficulty = random.randint(0,2)
-    filename = random.randint(1, numFilesInFolder[difficulty])
-
-    print(difficulty)
-    print(filename)
-
-    os.startfile(pathName + difficultyFolder[difficulty] + str(filename) + ".txt", "print")    
+    if (time.time() - lastFocusTime) > 2:
+        print("Focus Time!")
+        lastFocusTime = time.time()
+    
+    rndMax = random.randrange(0, len(currentList[DIFFICULTY_LEVEL]))
+    filename = currentList[DIFFICULTY_LEVEL][rndMax]
+    print(currentPath[DIFFICULTY_LEVEL] + filename)
+    os.startfile(currentPath[DIFFICULTY_LEVEL] + filename, "print")
+  
